8981821ad1516b29cf17b574972c35e1bd47b78b
aexaratechnologies-blip <aexaratechnologies@gmail.com>
2026-09-08 13:41:42 +0530

    Bump SELLB2 Android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.aexara.sellb2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     buildTypes {
         release {
