0fdc0f3b76f0906bb92a8966565ae607e087c9bd
aexaratechnologies-blip <aexaratechnologies@gmail.com>
2026-09-09 15:46:20 +0530

    Prepare SELLB2 Android APK v1.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.aexara.sellb2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
     buildTypes {
         release {
