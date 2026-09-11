8b1b0a07e7cf1f7480a34c7c8825a283a1134068
aexaratechnologies-blip <aexaratechnologies@gmail.com>
2026-09-11 11:29:49 +0530

    Bump SELLB2 production Android to 1.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.aexara.sellb2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.2"
+        versionCode = 7
+        versionName = "1.3.3"
     }
 
     signingConfigs {
