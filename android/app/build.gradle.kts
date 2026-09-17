6628ca0dbea858fc62d09c92da5aac5c7e6863f0
aexaratechnologies-blip <aexaratechnologies@gmail.com>
2026-09-17 12:20:26 +0530

    Set SELLB2 Android version to 2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.aexara.sellb2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.3.4"
+        versionCode = 9
+        versionName = "2.0"
     }
 
     signingConfigs {
